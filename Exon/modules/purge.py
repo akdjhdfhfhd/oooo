b9545209c138af7b40ef9b8d55f6c24b8aa8a67d
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-03-01 07:34:03 +0300

    Update purge.py

--- Exon/modules/purge.py
+++ Exon/modules/purge.py
@@ -18,7 +18,7 @@ async def purge_messages(event):
 
     if not await user_is_admin(
         user_id=event.sender_id, message=event
-    ) and event.from_id not in [1087968824]:
+    ) and event.from_id not in [1748768168]:
         await event.reply("ᴏɴʟʏ ᴀᴅᴍɪɴs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
         return
 
@@ -60,7 +60,7 @@ async def delete_messages(event):
 
     if not await user_is_admin(
         user_id=event.sender_id, message=event
-    ) and event.from_id not in [1087968824]:
+    ) and event.from_id not in [1748768168]:
         await event.reply("ᴏɴʟʏ ᴀᴅᴍɪɴs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
         return
 
